2e5b4c52f9a9340c056b8da24d6d9d4138446764
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 16:57:37 -0300

    Update jobs_manager.js

--- ~gamemaster_panel/managers/jobs_manager.js
+++ ~gamemaster_panel/managers/jobs_manager.js
@@ -1,1 +1,11 @@
+import { db } from '../../shared/firebase_config.js';
+
+console.log('Jobs Manager loaded');
+
+function initManager() {
+    const content = document.getElementById('manager-content');
+    content.innerHTML = '<p>Jobs Manager ready. You can create jobs or quests here.</p>';
+}
+
+initManager();
 
