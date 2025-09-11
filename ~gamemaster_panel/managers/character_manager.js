01fd63edb02a1cbb0140b6c05061bd9bd66c186f
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 16:56:51 -0300

    Update character_manager.js

--- ~gamemaster_panel/managers/character_manager.js
+++ ~gamemaster_panel/managers/character_manager.js
@@ -1,1 +1,11 @@
+import { db } from '../../shared/firebase_config.js';
+
+console.log('Character Manager loaded');
+
+function initManager() {
+    const content = document.getElementById('manager-content');
+    content.innerHTML = '<p>Character Manager ready. You can manage characters here.</p>';
+}
+
+initManager();
 
