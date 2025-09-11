0eaba4594a805325b5a6f8052ae121bb566fbd0c
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 16:43:50 -0300

    Update firebase_config.js

--- shared/firebase_config.js
+++ shared/firebase_config.js
@@ -1,1 +1,18 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+// 🔥 replace with your own Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyAReb65bsaoa145J5Yu2R61nrr4vvmqBiQ",
+    authDomain: "demand-rusher.firebaseapp.com",
+    projectId: "demand-rusher",
+    storageBucket: "demand-rusher.firebasestorage.app",
+    messagingSenderId: "491613307536",
+    appId: "1:491613307536:web:52f3339b4f12ef7418ce15",
+    measurementId: "G-VY4JSL70QV"
+};
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
