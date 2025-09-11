6afff1af5bfaf1731e421554958858ffed33e5d9
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 16:58:01 -0300

    Update resources_manager.js

--- ~gamemaster_panel/managers/resources_manager.js
+++ ~gamemaster_panel/managers/resources_manager.js
@@ -1,1 +1,11 @@
+import { db } from '../../shared/firebase_config.js';
+
+console.log('Resources Manager loaded');
+
+function initManager() {
+    const content = document.getElementById('manager-content');
+    content.innerHTML = '<p>Resources Manager ready. You can manage game resources here.</p>';
+}
+
+initManager();
 
