d2ad3807505192268f743e5a043bbfad09c77782
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 16:17:50 -0300

    Update firebase-config.js
    updated firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase
 
 // 🔥 replace with your own Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAReb65bsaoa145J5Yu2R61nrr4vvmqBiQ",
+    authDomain: "demand-rusher.firebaseapp.com",
+    projectId: "demand-rusher",
+    storageBucket: "demand-rusher.firebasestorage.app",
+    messagingSenderId: "491613307536",
+    appId: "1:491613307536:web:52f3339b4f12ef7418ce15",
+    measurementId: "G-VY4JSL70QV"
 };
 
 // Initialize
