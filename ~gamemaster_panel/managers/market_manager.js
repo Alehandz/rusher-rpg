9a0e8280b2a69fe717de4b26d4372350dc814b60
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 16:57:12 -0300

    Update market_manager.js

--- ~gamemaster_panel/managers/market_manager.js
+++ ~gamemaster_panel/managers/market_manager.js
@@ -1,1 +1,11 @@
+import { db } from '../../shared/firebase_config.js';
+
+console.log('Market Manager loaded');
+
+function initManager() {
+    const content = document.getElementById('manager-content');
+    content.innerHTML = '<p>Market Manager ready. You can manage market offers here.</p>';
+}
+
+initManager();
 
