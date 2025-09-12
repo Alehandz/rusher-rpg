86abcf508e9cbed1ef0ae419c15f6cdfcfddadc4
Alehandz <alexandre.geisler@gmail.com>
2025-09-11 21:28:36 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,21 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// Importando apenas o que precisamos do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-// 🔥 replace with your own Firebase config
+// Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAReb65bsaoa145J5Yu2R61nrr4vvmqBiQ",
-    authDomain: "demand-rusher.firebaseapp.com",
-    projectId: "demand-rusher",
-    storageBucket: "demand-rusher.firebasestorage.app",
-    messagingSenderId: "491613307536",
-    appId: "1:491613307536:web:52f3339b4f12ef7418ce15",
-    measurementId: "G-VY4JSL70QV"
+  apiKey: "AIzaSyAReb65bsaoa145J5Yu2R61nrr4vvmqBiQ",
+  authDomain: "demand-rusher.firebaseapp.com",
+  projectId: "demand-rusher",
+  storageBucket: "demand-rusher.firebasestorage.app",
+  messagingSenderId: "491613307536",
+  appId: "1:491613307536:web:52f3339b4f12ef7418ce15",
+  measurementId: "G-VY4JSL70QV"
 };
 
-// Initialize
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const auth = getAuth(app);
 
+// Exporta apenas o que vamos usar
+export { app, auth };
